0ec5d8939fb647468529050b3477c50e1c1c7077
chryd <106443732+chryd@users.noreply.github.com>
2026-09-11 19:10:25 -0400

    Update test_calculator.py

--- src/tests/test_calculator.py
+++ src/tests/test_calculator.py
@@ -11,4 +11,34 @@ def test_app():
     welcome_message = my_calculator.get_hello_message()
     assert "== Calculatrice v1.0 ==" in welcome_message
 
-# TODO: ajoutez les tests
+def test_add_success():
+    my_calculator = Calculator()
+    assert my_calculator.addition(2, 3) == 5
+
+# def test_add_failure():
+#     my_calculator = Calculator()
+#     assert my_calculator.addition(2, 3) == 6
+
+def test_sub_success():
+    my_calculator = Calculator()
+    assert my_calculator.subtraction(5, 3) == 2
+
+# def test_sub_failure():
+#     my_calculator = Calculator()
+#     assert my_calculator.subtraction(5, 3) == 3
+
+def test_mult_success():
+    my_calculator = Calculator()
+    assert my_calculator.multiplication(2, 3) == 6
+
+# def test_mult_failure():
+#     my_calculator = Calculator()
+#     assert my_calculator.multiplication(2, 3) == 7
+
+def test_div_success():
+    my_calculator = Calculator()
+    assert my_calculator.division(6, 3) == 2
+
+# def test_div_failure():
+#     my_calculator = Calculator()
+#     assert my_calculator.division(6, 3) == 3
